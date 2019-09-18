fa808c49acabe98803ea9dc1eff3781026ae96da
Airthium3 <quentin.tessier@epitech.eu>
2019-09-18 14:31:04 +0200

    Formatted and documented all files in src/util

--- src/util/Array.h
+++ src/util/Array.h
@@ -1,3 +1,7 @@
+/**
+ * @file Array.h
+ * @brief Simple abstraction over a bare bone C array.
+ */
 #ifndef ARRAY_H_
 #define ARRAY_H_
 
@@ -30,20 +34,72 @@ inline bool isArrayReady(Array a) { return (a.Data) ? true : false; }
 /**
  * @brief Create a new Array.
  *
- * @param size_t ElementCount [in] - Number of elements in the Array.
- * @param size_t ElementSize [in] - Size of one element (eg. sizeof(int)).
+ * @param ElementCount [in] - Number of elements in the Array.
+ * @param ElementSize [in] - Size of one element (eg. sizeof(int)).
  *
- * @return Array - Allocate a new Array (use ffGraph::isArrayReady to check return value).
+ * @return ffGraph::Array - Allocate a new ffGraph::Array (use ffGraph::isArrayReady to check return value).
  */
-inline Array ffNewArray(size_t ElementCount, size_t ElementSize) { return {ElementCount, ElementSize, malloc(ElementSize * ElementCount)}; };
+inline Array ffNewArray(size_t ElementCount, size_t ElementSize) {
+    return {ElementCount, ElementSize, malloc(ElementSize * ElementCount)};
+};
 
 /**
  * @brief Release Array memory and reset it.
  *
- * @param Array [in] - Array to destroy.
+ * @param a [in] - ffGraph::Array to destroy.
  */
-inline void ffDestroyArray(Array a) { free(a.Data); memset(&a, 0, sizeof(Array)); }
+inline void ffDestroyArray(Array a) {
+    free(a.Data);
+    memset(&a, 0, sizeof(Array));
+}
 
-} // namespace ffGraph
+/**
+ * @brief Copy size bytes to a array memory from a src opaque pointer
+ *
+ * @param dst [out] - ffGraph::Array where the data is copied.
+ * @param src [in] - Source memory.
+ * @param size [in] - Number of bytes copied.
+ */
+inline void ffMemcpyArray(Array dst, const void *src, size_t size) { memcpy(dst.Data, src, size); }
 
-#endif // ARRAY_H_
+/**
+ * @brief Copy ffGraph::Array's size bytes to a array memory from a src opaque pointer
+ *
+ * @param dst [out] - ffGraph::Array where the data is copied.
+ * @param src [in] - Source memory.
+ */
+inline void ffMemcpyArray(Array dst, const void *src) { memcpy(dst.Data, src, dst.ElementCount * dst.ElementSize); }
+
+/**
+ * @brief Copy size bytes to a array memory from a src ffGraph::Array
+ *
+ * @param dst [out] - ffGraph::Array where the data is copied.
+ * @param src [in] - Source ffGraph::Array.
+ * @param size [in] - Number of bytes copied.
+ */
+inline void ffMemcpyArray(Array dst, Array src, size_t size) { memcpy(dst.Data, src.Data, size); }
+
+/**
+ * @brief Copy ffGraph::Array's size bytes to a array memory from a src ffGraph::Array
+ *
+ * @param dst [out] - ffGraph::Array where the data is copied.
+ * @param src [in] - Source ffGraph::Array.
+ */
+inline void ffMemcpyArray(Array dst, Array src) { memcpy(dst.Data, src.Data, dst.ElementCount * dst.ElementSize); }
+
+/**
+ * @brief Apply function "void (* FUNCTION)(void *, size_t)" to each array element.
+ * Function throws a warning at compilation, "pointer of type ‘void *’ used in arithmetic".
+ *
+ * @param a [in] - ffGraph::Array which the function will run on.
+ * @param func [in] - Function pointer
+ */
+inline void ffArrayForEach(Array a, void (*func)(void *array, size_t idx)) {
+    for (size_t idx = 0; idx < a.ElementCount; ++idx) {
+        func(a.Data + (idx * a.ElementSize), idx);
+    }
+}
+
+}    // namespace ffGraph
+
+#endif    // ARRAY_H_
--- src/util/Logger.h
+++ src/util/Logger.h
@@ -1,22 +1,62 @@
+/**
+ * @file Logger.h
+ * @brief Allow the application to report bugs and other informations.
+ */
 #ifndef LOGGER_H_
 #define LOGGER_H_
 
 #include <cassert>
 #include <cstdarg>
 #include <cstring>
-#include <ctime>
 #include <iostream>
 #include <string>
 
 namespace ffGraph {
+/**
+ * @brief Internal function to the macro ffGraph::GetCurrentLogLocation().
+ *
+ * @param file [in] - __FILE__ macro, gives the current file location.
+ * @param line [in] - __LINE__ macro, gives the current line in the file.
+ *
+ * @return std::string - Returns a string formatted this way "${__FILE__} at line ${__LINE__} => ".
+ */
+inline std::string GetCurrentLogLocation_internal(const char *file, int line) {
+    std::string ret(file);
+    ret.append(" at line ");
+    ret.append(std::to_string(line));
+    ret.append(" => ");
+    return ret;
+}
 
-inline std::string GetCurrentLogLocation_internal(const char *file, int line) { std::string ret(file); ret.append(" at line "); ret.append(std::to_string(line)); return ret; }
-#define GetCurrentLogLocation() GetCurrentLogLocation_internal(__FILE__, __LINE__)
+/**
+ * @brief Calls ffGraph::GetCurrentLogLocation_internal(), gets the position of this macro call in a file.
+ * Giving exactly where the log function was called.
+ *
+ * @return std::string - Returns a string formatted this way "${__FILE__} at line ${__LINE__} => ".
+ */
+#define GetCurrentLogLocation( ) GetCurrentLogLocation_internal(__FILE__, __LINE__)
 
+/**
+ * @brief Takes a variable as input and make it's a string out of it.
+ *
+ * @return const char[] - A string made of the name of a variable (eg: GetVariableAsString(SomeEnumName) gives
+ * "SomeEnumName").
+ */
+#define GetVariableAsString(Var) #Var
+
+/**
+ * @brief Logs a Error. When compiled in Debug, this log call will assert.
+ *
+ * @param Location [in] - Location of the log call, can be custom or ffGraph::GetCurrentLogLocation().
+ * @param Format [in] - Printf like formatting string.
+ * @param ... [in] - Printf like va_list.
+ *
+ * @return void
+ */
 inline void LogError(std::string Location, const char *Format, ...) {
     va_list list;
     va_start(list, Format);
-    printf("ERROR :: Location -> %s | Message :\n", Location.data());
+    printf("ERROR :: Location -> %s | Message : ", Location.data( ));
     vprintf(Format, list);
     printf("\n");
     va_end(list);
@@ -24,24 +64,43 @@ inline void LogError(std::string Location, const char *Format, ...) {
     assert(0);
 #endif
 }
+
+/**
+ * @brief Logs a Warning.
+ *
+ * @param Location [in] - Location of the log call, can be custom or ffGraph::GetCurrentLogLocation().
+ * @param Format [in] - Printf like formatting string.
+ * @param ... [in] - Printf like va_list.
+ *
+ * @return void
+ */
 inline void LogWarning(std::string Location, const char *Format, ...) {
     va_list list;
     va_start(list, Format);
-    printf("WARNING :: Location -> %s | Message :\n", Location.data());
+    printf("WARNING :: Location -> %s | Message : ", Location.data( ));
     vprintf(Format, list);
     printf("\n");
     va_end(list);
 }
 
+/**
+ * @brief Logs a Information.
+ *
+ * @param Location [in] - Location of the log call, can be custom or ffGraph::GetCurrentLogLocation().
+ * @param Format [in] - Printf like formatting string.
+ * @param ... [in] - Printf like va_list.
+ *
+ * @return void
+ */
 inline void LogInfo(std::string Location, const char *Format, ...) {
     va_list list;
     va_start(list, Format);
-    printf("INFORMATION :: Location -> %s | Message :\n", Location.data());
+    printf("INFORMATION :: Location -> %s | Message : ", Location.data( ));
     vprintf(Format, list);
     printf("\n");
     va_end(list);
 }
 
-} // namespace ffGraph
+}    // namespace ffGraph
 
 #endif    // LOGGER_H_
